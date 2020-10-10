6d6e9b6c54a1b6aed37b52797ce41a3a939f5b05
Douglas Campbell <dacampbelltbone@gmail.com>
2020-10-10 03:36:53 -0400

    Add Vue to app. Mounted app.

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = "Socks";
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "Made with no seams providing maximum comfort.",
+    };
+  },
+});
